bdef7b8a5d09dd1542373473f66f98003aa57e22
Daniele Roccaforte <64607921+YonderUrik@users.noreply.github.com>
2024-03-24 22:18:15 +0100

    update apple-app-site-association

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -19,5 +19,15 @@ module.exports = {
     }
 
     return config
+  },
+  experimental: {
+    headers() {
+      return [
+        {
+          source: '/.well-known/apple-app-site-association',
+          headers: [{ key: 'content-type', value: 'application/json' }]
+        }
+      ]
+    }
   }
 }
