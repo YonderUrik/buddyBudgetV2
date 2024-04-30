c5e46ccd90b39bf0a404d526d977367e0e19d96d
Daniele Roccaforte <64607921+YonderUrik@users.noreply.github.com>
2024-04-30 10:17:52 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ VERSION=2.2.0
 
 echo "Building buddy-budget image. You have 5 seconds to stop the script"
 sleep 5
-docker build ./backend -f backend/Dockerfile -t buddy-budget:$VERSION
+docker build ./backend -f backend/Dockerfile -t buddy-budget-backend:$VERSION
 docker image push buddy-budget-backend:$VERSION
 
-docker build ./stocks_manager -f stocks_manager/Dockerfile -t buddy-budget:$VERSION
+docker build ./stocks_manager -f stocks_manager/Dockerfile -t buddy-budget-stock:$VERSION
 docker image push buddy-budget-stock:$VERSION
 
